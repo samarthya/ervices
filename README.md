8c957e21ffb253201acd0ec6d7ecbeb8a6487a0e
Saurabh Sharma <saurabh777@gmail.com>
2021-05-27 11:50:43 +0530

    Version 2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Simple webservice to test helm chart
+# Simple webservice
+As I try and up my knowledge on the helm subject, I just wanted to test the deployment
+
+## endpoints
+The end points exposed in different version
+
+### 1. /hello
+Simple get should return a default output as 
+
+```json
+{
+    "message" : "Hello from server"
+}
+```
--- src/ws/ws.go
+++ src/ws/ws.go
@@ -1,1 +1,14 @@
 package ws
+
+import "encoding/json"
+
+// HelloMessage Aa simple structure that will have the hello message
+type HelloMessage struct {
+	//Message that needs to be returned
+	Message string "json:message"
+}
+
+func HelloFromServer() ([]byte, error) {
+	var helloMessage = HelloMessage{Message: "Hello from server!"}
+	return json.Marshal(helloMessage)
+}
